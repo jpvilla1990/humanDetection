d2deb89f2c6285127d924b520a7c90e964d8672c
Juan Pablo Villa Serna <jpvilla1990@gmail.com>
2021-07-03 12:26:25 +0200

    server

--- server/AIbackend/AIbackend/settings.py
+++ server/AIbackend/AIbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$_p@k$8sw6p87bg4d1fupk^rs$gtjwyab%cfpg9ce1@(%3vkql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
